b91edc1a3309d15e699ef498750e710ec1f50e44
SOURYA DEY <souryadey031@gmail.com>
2024-06-11 03:00:13 +0530

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "../index.html"
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
